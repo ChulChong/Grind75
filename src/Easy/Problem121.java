65071131cdd5f75b99c236978b310a864485986b
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2025-07-30 22:00:52 -0400

    problem 121 solved

---
+++ src/Easy/Problem121.java
@@ -0,0 +1,20 @@
+package Easy;
+
+public class Problem121 {
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.println(maxProfit(prices));
+    }
+
+    public static int maxProfit(int[] prices) {
+        int MinPriceSofar = prices[0];
+        int MaxProfit = 0;
+        for (int i : prices) {
+            if (i < MinPriceSofar) {
+                MinPriceSofar = i;
+            }
+            MaxProfit = Math.max(MaxProfit, i - MinPriceSofar);
+        }
+        return MaxProfit;
+    }
+}
