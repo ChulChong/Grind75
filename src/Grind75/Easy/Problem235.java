45b62c9cd8e369881c80c2d294ca30a632b678d7
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2026-03-30 17:40:00 -0400

    problem 235 solved

---
+++ src/Grind75/Easy/Problem235.java
@@ -0,0 +1,32 @@
+package Grind75.Easy;
+
+import Grind75.TreeNode;
+import com.sun.source.tree.Tree;
+
+public class Problem235 {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(6,
+                new TreeNode(2,
+                        new TreeNode(0), new TreeNode(4, new TreeNode(3), new TreeNode(5))),
+                new TreeNode(8, new TreeNode(7), new TreeNode(9)));
+        TreeNode p = new TreeNode(2);
+        TreeNode q = new TreeNode(8);
+
+        TreeNode result = lowestCommonAncestor(root, p, q);
+        System.out.println(result.val);
+    }
+
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode current = root;
+        while (current != null) {
+            if (current.val > p.val && current.val > q.val) {
+                current = current.left;
+            } else if (current.val < p.val && current.val < q.val) {
+                current = current.right;
+            } else {
+                return current;
+            }
+        }
+        return null;
+    }
+}
