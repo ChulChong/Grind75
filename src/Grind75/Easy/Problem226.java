613fd836c502ba459b96d6262a2b6b4c165c39b4
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2026-03-30 10:50:48 -0400

    Problem 226 solved

---
+++ src/Grind75/Easy/Problem226.java
@@ -0,0 +1,30 @@
+package Grind75.Easy;
+
+import Grind75.TreeNode;
+
+public class Problem226 {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+
+        TreeNode.printTree(invertTree(root));
+    }
+
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
