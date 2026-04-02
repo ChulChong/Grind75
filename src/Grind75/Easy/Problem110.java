7150c4a74087007f2f5d3da06db70af0eaa78ddf
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2026-04-01 22:39:35 -0400

    Problem 110 solved

--- src/Grind75/Easy/Problem110.java
+++ src/Grind75/Easy/Problem110.java
@@ -9,10 +9,22 @@ public class Problem110 {
     }
 
     public static boolean isBalanced(TreeNode root) {
-        if (root == null) return true;
-        if (root.left != null && root.right != null) {
 
+        return getHeight(root) != -1;
+    }
+
+    private static int getHeight(TreeNode node) {
+        if (node == null) return 0;
+
+        int leftHeight = getHeight(node.left);
+        if (leftHeight == -1) return -1;
+        int rightHeight = getHeight(node.right);
+        if (rightHeight == -1) return -1;
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
         }
-        return false;
+
+        return Math.max(leftHeight, rightHeight) + 1;
     }
 }
