f855740428440c313c4b1a4fd31eb08433176b33
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2026-04-03 10:31:56 -0400

    Problem 232 solved

---
+++ src/Grind75/Easy/MyQueue.java
@@ -0,0 +1,38 @@
+package Grind75.Easy;
+
+import java.util.Stack;
+
+public class MyQueue {
+    Stack<Integer> inStack = new Stack<>();
+    Stack<Integer> outStack = new Stack<>();
+
+    public MyQueue() {
+
+    }
+
+    public void push(int x) {
+        inStack.push(x);
+    }
+
+    public int pop() {
+        if (outStack.isEmpty()) {
+            while (!inStack.isEmpty()) {
+                outStack.push(inStack.pop());
+            }
+        }
+        return outStack.pop();
+    }
+
+    public int peek() {
+        if (outStack.isEmpty()) {
+            while (!inStack.isEmpty()) {
+                outStack.push(inStack.pop());
+            }
+        }
+        return outStack.peek();
+    }
+
+    public boolean empty() {
+        return inStack.isEmpty() && outStack.isEmpty();
+    }
+}
