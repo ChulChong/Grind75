a95fc8f3462c284dff23495f3924435a9d283a11
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2026-04-04 10:41:51 -0400

    reviewed problem 21

---
+++ src/Grind75/Review/Problem21.java
@@ -0,0 +1,35 @@
+package Grind75.Review;
+
+
+import Grind75.ListNode;
+
+public class Problem21 {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+        ListNode.printNode(mergeTwoLists(l1, l2));
+    }
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+        if (list1 == null) {
+            curr.next = list2;
+        } else {
+            curr.next = list1;
+        }
+        return dummy.next;
+    }
+}
+
