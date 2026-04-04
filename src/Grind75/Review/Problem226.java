6d176a71369c497856c7f6c0c40a3cb69cb424b9
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2026-04-04 14:27:25 -0400

    problem  226 reviewed

---
+++ src/Grind75/Review/Problem226.java
@@ -0,0 +1,28 @@
+package Grind75.Review;
+
+import Grind75.TreeNode;
+
+public class Problem226 {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+
+        TreeNode.printTree(invertTree(root));
+    }
+
+    public static TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+        TreeNode temp = null;
+        temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
