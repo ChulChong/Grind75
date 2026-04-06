76086596d1d6ef145c676b4c6a2869ca59526c35
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2026-04-06 16:59:38 -0400

    problem 232 reviewed

---
+++ src/Grind75/Review/Problem232.java
@@ -0,0 +1,38 @@
+package Grind75.Review;
+
+import java.util.Stack;
+
+public class Problem232 {
+    Stack<Integer> inStack = new Stack<>();
+    Stack<Integer> outStack = new Stack<>();
+
+    public Problem232() {
+
+    }
+
+    public void push(int x) {
+        inStack.push(x);
+    }
+
+    public int pop() {
+        if (outStack.isEmpty()) {
+            while (!inStack.isEmpty()) {
+                outStack.push(inStack.pop());
+            }
+        }
+        return outStack.pop();
+    }
+
+    public int peek() {
+        if (outStack.isEmpty()) {
+            while (!inStack.isEmpty()) {
+                outStack.push(inStack.pop());
+            }
+        }
+        return outStack.peek();
+    }
+
+    public boolean empty() {
+        return inStack.isEmpty() && outStack.isEmpty();
+    }
+}
