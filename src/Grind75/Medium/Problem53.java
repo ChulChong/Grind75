180185134256c4907d2542d97a0a566a1983b004
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2026-04-11 14:28:49 -0400

    Problem 53 solved

---
+++ src/Grind75/Medium/Problem53.java
@@ -0,0 +1,18 @@
+package Grind75.Medium;
+
+public class Problem53 {
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(maxSubArray(nums));
+    }
+
+    public static int maxSubArray(int[] nums) {
+        int currSum = nums[0];
+        int maxSum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            currSum = Math.max(nums[i], currSum + nums[i]);
+            maxSum = Math.max(currSum, maxSum);
+        }
+        return maxSum;
+    }
+}
