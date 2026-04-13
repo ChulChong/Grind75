b2bed7e5fb8bcedb14292b93ddd966c225ad2597
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2026-04-13 11:38:50 -0400

    Problem 49 solved

---
+++ src/Grind75/Medium/Problem49.java
@@ -0,0 +1,25 @@
+package Grind75.Medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class Problem49 {
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> hm = new HashMap<>();
+        for (String s : strs) {
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+            hm.putIfAbsent(key, new ArrayList<>());
+            hm.get(key).add(s);
+        }
+        return new ArrayList<>(hm.values());
+    }
+}
