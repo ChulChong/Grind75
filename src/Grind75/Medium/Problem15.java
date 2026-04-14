cc626c939248fd7f682d51c1817f35dd52edb880
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2026-04-13 20:26:12 -0400

    Problem 15 solved

--- src/Grind75/Medium/Problem15.java
+++ src/Grind75/Medium/Problem15.java
@@ -1,2 +1,38 @@
-package Grind75.Medium;public class Problem15 {
+package Grind75.Medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Problem15 {
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        System.out.println(threeSum(nums));
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            if (nums[i] > 0) break;
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                if (nums[left] + nums[right] + nums[i] > 0) {
+                    right--;
+                } else if (nums[left] + nums[right] + nums[i] < 0) {
+                    left++;
+                } else if (nums[left] + nums[right] + nums[i] == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                }
+            }
+        }
+        return ans;
+    }
 }
--- src/Grind75/Review/Problem125.java
+++ src/Grind75/Review/Problem125.java
@@ -7,21 +7,21 @@ public class Problem125 {
     }
 
     public static boolean isPalindrome(String s) {
-        int leftIndex = 0;
-        int rightIndex = s.length() - 1;
-        while (leftIndex < rightIndex) {
-            if (!Character.isLetterOrDigit(s.charAt(leftIndex))) {
-                leftIndex = leftIndex + 1;
-            } else if (!Character.isLetterOrDigit(s.charAt(rightIndex))) {
-                rightIndex = rightIndex - 1;
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            char leftCharacter = s.charAt(left);
+            char rightCharacter = s.charAt(right);
+            if (!Character.isLetterOrDigit(leftCharacter)) {
+                left++;
+            } else if (!Character.isLetterOrDigit(rightCharacter)) {
+                right--;
             } else {
-                if (Character.toLowerCase(s.charAt(leftIndex)) != Character.toLowerCase(s.charAt(rightIndex))) {
-                    return false;
-                }
-                leftIndex++;
-                rightIndex--;
+                if (Character.toLowerCase(leftCharacter) == Character.toLowerCase(rightCharacter)) {
+                    left++;
+                    right--;
+                } else return false;
             }
-
         }
         return true;
     }
