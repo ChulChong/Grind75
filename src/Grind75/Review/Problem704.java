65e6b4d3050f8fb39c95ab3f79570556491c9cf3
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2026-04-15 22:20:13 -0400

    Problem 704

--- src/Grind75/Review/Problem704.java
+++ src/Grind75/Review/Problem704.java
@@ -11,13 +11,10 @@ public class Problem704 {
         int left = 0;
         int right = nums.length - 1;
         while (left <= right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] == target) return mid;
-            if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) left = mid + 1;
+            else if (nums[mid] > target) right = mid - 1;
+            else return mid;
         }
         return -1;
     }
