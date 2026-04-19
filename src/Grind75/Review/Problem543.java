8d33b2dff8119393bd2b1903d2b8a0e1a6eae11a
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2026-04-19 16:33:16 -0400

    Problem 543 reviwed

---
+++ src/Grind75/Review/Problem543.java
@@ -0,0 +1,25 @@
+package Grind75.Review;
+
+import Grind75.TreeNode;
+
+public class Problem543 {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3));
+        System.out.println(diameterOfBinaryTree(root));
+    }
+
+    public static int longestDiameter = 0;
+
+    public static int diameterOfBinaryTree(TreeNode root) {
+        helper(root);
+        return longestDiameter;
+    }
+
+    private static int helper(TreeNode root) {
+        if (root == null) return 0;
+        int left = helper(root.left);
+        int right = helper(root.right);
+        longestDiameter = Math.max((left + right), longestDiameter);
+        return Math.max(left, right) + 1;
+    }
+}
