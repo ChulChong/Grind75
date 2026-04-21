2edff91b84b7a63c1c75901d8946a8d634085ad0
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2026-04-20 21:05:04 -0400

    Problem 572 solved

---
+++ src/Grind75/Easy/Problem572.java
@@ -0,0 +1,26 @@
+package Grind75.Easy;
+
+import Grind75.TreeNode;
+
+public class Problem572 {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3, new TreeNode(4, new TreeNode(1), new TreeNode(2)), new TreeNode(5));
+        TreeNode subRoot = new TreeNode(4, new TreeNode(1), new TreeNode(2));
+        System.out.println(isSubtree(root, subRoot));
+    }
+
+    public static boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) return false;
+        if (isSameTree(root, subRoot)) return true;
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    public static boolean isSameTree(TreeNode root, TreeNode subRoot) {
+        if (root == null && subRoot == null) return true;
+        if (root == null || subRoot == null) return false;
+        boolean left = isSameTree(root.left, subRoot.left);
+        boolean right = isSameTree(root.right, subRoot.right);
+        return left && right && root.val == subRoot.val;
+    }
+
+}
