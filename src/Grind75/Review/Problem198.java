fb25134871ecd7a076fbec534f25d171e841543d
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2026-04-23 15:17:12 -0400

    Problem 198 reviwed

---
+++ src/Grind75/Review/Problem198.java
@@ -0,0 +1,22 @@
+package Grind75.Review;
+
+public class Problem198 {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+        System.out.println(rob(nums));
+    }
+
+    public static int rob(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        int first = nums[0];
+        int second = Math.max(nums[0], nums[1]);
+        if (nums.length == 2) return second;
+        for (int i = 2; i < nums.length; i++) {
+            int curr = Math.max(second, first + nums[i]);
+            first = second;
+            second = curr;
+        }
+
+        return Math.max(first, second);
+    }
+}
