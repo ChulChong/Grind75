83e35ec1b734ed0c015f63b936541558b6df0c52
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2026-04-23 17:25:59 -0400

    Problem 20 reviewed

--- src/Grind75/Review/Problem20.java
+++ src/Grind75/Review/Problem20.java
@@ -16,10 +16,10 @@ public class Problem20 {
                 st.push(c);
             } else {
                 if (st.isEmpty()) return false;
-                char top = st.pop();
-                if (c == ')' && top != '(') return false;
-                else if (c == ']' && top != '[') return false;
-                else if (c == '}' && top != '{') return false;
+                char stPop = st.pop();
+                if (c == ')' && stPop != '(' || c == '}' && stPop != '{' || c == ']' && stPop != '[') {
+                    return false;
+                }
             }
         }
         return st.isEmpty();
