76d92560d98213bf5ec9f8727cd52aa45e50aa43
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2026-04-23 17:53:52 -0400

    Problem 21 reviwed

--- src/Grind75/Review/Problem21.java
+++ src/Grind75/Review/Problem21.java
@@ -14,6 +14,7 @@ public class Problem21 {
     public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode dummy = new ListNode(-1);
         ListNode curr = dummy;
+
         while (list1 != null && list2 != null) {
             if (list1.val <= list2.val) {
                 curr.next = list1;
@@ -24,11 +25,9 @@ public class Problem21 {
             }
             curr = curr.next;
         }
-        if (list1 == null) {
-            curr.next = list2;
-        } else {
-            curr.next = list1;
-        }
+        if (list1 == null) curr.next = list2;
+        if (list2 == null) curr.next = list1;
+
         return dummy.next;
     }
 }
