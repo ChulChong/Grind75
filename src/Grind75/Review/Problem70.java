ec58a524967165c06a1b7cb1367420a0b0a007c7
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2026-04-25 17:53:47 -0400

    Problem70 reviewed

--- src/Grind75/Review/Problem70.java
+++ src/Grind75/Review/Problem70.java
@@ -7,16 +7,15 @@ public class Problem70 {
     }
 
     public static int climbStairs(int n) {
-        int first = 1;
-        int second = 2;
-        if (n == 1) return first;
-        if (n == 2) return second;
-        int totalStep = 0;
-        for (int i = 2; i < n; i++) {
-            totalStep = first + second;
-            first = second;
-            second = totalStep;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        int init = 1;
+        int prev = 2;
+        for (int i = 3; i <= n; i++) {
+            int curr = init + prev;
+            init = prev;
+            prev = curr;
         }
-        return totalStep;
+        return prev;
     }
 }
