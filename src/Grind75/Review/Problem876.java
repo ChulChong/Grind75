6656d5744fa1dc76911ae7ad6904925ea0dcc2f3
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2026-04-26 16:56:15 -0400

    Problem 876 reviewed

---
+++ src/Grind75/Review/Problem876.java
@@ -0,0 +1,22 @@
+package Grind75.Review;
+
+import Grind75.ListNode;
+
+public class Problem876 {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        ListNode.printNode(middleNode(head));
+    }
+
+    public static ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+
+}
