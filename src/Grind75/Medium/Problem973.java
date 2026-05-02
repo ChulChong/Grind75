ddab7a74ebbc3dd873b427205cbf9b567047b207
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2026-05-02 13:23:20 -0400

    Problem 973 solved

---
+++ src/Grind75/Medium/Problem973.java
@@ -0,0 +1,30 @@
+package Grind75.Medium;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
+public class Problem973 {
+    public static void main(String[] args) {
+        int[][] points = {{3, 3}, {5, -1}, {-2, 4}};
+        int k = 2;
+        System.out.println(Arrays.deepToString(kClosest(points, k)));
+    }
+
+    public static int[][] kClosest(int[][] points, int k) {
+        int[][] ans = new int[k][points[0].length];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            int distA = a[0] * a[0] + a[1] * a[1];
+            int distB = b[0] * b[0] + b[1] * b[1];
+            return distA - distB;
+        });
+
+        for (int[] point : points) {
+            pq.add(point);
+        }
+        for (int i = 0; i < k; i++) {
+            ans[i] = pq.poll();
+        }
+        return ans;
+    }
+}
