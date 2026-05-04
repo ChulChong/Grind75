657469d52b140fb218816424f4867b6c3910548e
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2026-05-03 20:11:04 -0400

    Problem 102 solved

---
+++ src/Grind75/Medium/Problem102.java
@@ -0,0 +1,34 @@
+package Grind75.Medium;
+
+import Grind75.TreeNode;
+
+import java.util.*;
+
+public class Problem102 {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+        System.out.println(levelOrder(root));
+    }
+
+
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) return ans;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            ans.add(list);
+        }
+        return ans;
+    }
+
+
+}
