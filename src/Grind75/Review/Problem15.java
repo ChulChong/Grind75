8a3418309e5d64025f60c93b14f159cdbfdff0ed
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2026-05-09 19:13:20 -0400

    Problem 15 reviwed

--- src/Grind75/Review/Problem15.java
+++ src/Grind75/Review/Problem15.java
@@ -9,19 +9,19 @@ public class Problem15 {
     }
 
     public static List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
+        List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
             int left = i + 1;
             int right = nums.length - 1;
             if (i > 0 && nums[i] == nums[i - 1]) continue;
             while (left < right) {
-                if (nums[i] + nums[left] + nums[right] > 0) {
-                    right--;
-                } else if (nums[i] + nums[left] + nums[right] < 0) {
+                if (nums[left] + nums[right] + nums[i] < 0) {
                     left++;
+                } else if (nums[left] + nums[right] + nums[i] > 0) {
+                    right--;
                 } else {
-                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    list.add(Arrays.asList(nums[i], nums[left], nums[right]));
                     left++;
                     right--;
                     while (left < right && nums[left] == nums[left - 1]) left++;
@@ -29,6 +29,6 @@ public class Problem15 {
                 }
             }
         }
-        return ans;
+        return list;
     }
 }
