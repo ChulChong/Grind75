705115ab4e873fc7d417d41a50aa77f149897b40
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2026-05-09 14:09:44 -0400

    Problem 57 solved

--- src/Grind75/Review/Problem57.java
+++ src/Grind75/Review/Problem57.java
@@ -2,31 +2,33 @@ package Grind75.Review;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Problem57 {
     public static void main(String[] args) {
-        int[][] intervals = {{1, 3}, {6, 9}};
+        int[][] intervals = {{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}};
         int[] newInterval = {2, 5};
         System.out.println(Arrays.deepToString(insert(intervals, newInterval)));
     }
 
     public static int[][] insert(int[][] intervals, int[] newInterval) {
         ArrayList<int[]> list = new ArrayList<>();
-        boolean inserted = false;
-        for (int[] interval : intervals) {
-            if (newInterval[0] > interval[1]) {
-                list.add(interval);
-            } else if (newInterval[1] < interval[0]) {
-                if (!inserted) {
-                    list.add(newInterval);
-                    inserted = true;
-                }
-                list.add(interval);
-            } else {
-                newInterval = new int[]{Math.min(interval[0], newInterval[0]), Math.max(interval[1], newInterval[1])};
-            }
+        int i = 0;
+        while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+            list.add(intervals[i]);
+            i++;
         }
-        if (!inserted) list.add(newInterval);
+        while (i < intervals.length && intervals[i][1] >= newInterval[0] && intervals[i][0] <= newInterval[1]) {
+            newInterval = new int[]{Math.min(newInterval[0], intervals[i][0]), Math.max(newInterval[1], intervals[i][1])};
+            i++;
+        }
+        list.add(newInterval);
+
+        while (i < intervals.length && intervals[i][0] > newInterval[1]) {
+            list.add(intervals[i]);
+            i++;
+        }
         return list.toArray(new int[0][]);
     }
 
