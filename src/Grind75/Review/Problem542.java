ccca039b6ff1ca631333101bc217903cfb94f8c4
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2026-05-09 16:35:44 -0400

    Problem 542 reviewed

--- src/Grind75/Review/Problem542.java
+++ src/Grind75/Review/Problem542.java
@@ -21,19 +21,15 @@ public class Problem542 {
                 }
             }
         }
-
         int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
         while (!queue.isEmpty()) {
-            int[] j = queue.poll();
+            int[] current = queue.poll();
             for (int[] dir : dirs) {
-                int nr = j[0] + dir[0];
-                int nc = j[1] + dir[1];
+                int nr = current[0] + dir[0];
+                int nc = current[1] + dir[1];
                 if (nr < 0 || nr >= mat.length || nc < 0 || nc >= mat[0].length || mat[nr][nc] != -1) continue;
-                else {
-                    mat[nr][nc] = mat[j[0]][j[1]] + 1;
-                    queue.offer(new int[]{nr, nc});
-                }
+                mat[nr][nc] = mat[current[0]][current[1]] + 1;
+                queue.offer(new int[]{nr, nc});
             }
         }
         return mat;
